4577eb97f8dadfc5f4d8a1734a3d20f0bb15ca59
pavelxhukin <pavel_xhukin@mail.ru>
2024-11-14 22:04:26 +0300

    roomNumber IS A NUMBER

--- timeTableProject/table/serialazers.py
+++ timeTableProject/table/serialazers.py
@@ -1,9 +1,10 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from table.models import Lesson
 
 class TableSerializer(ModelSerializer):
-    
+    inRoom = serializers.SlugRelatedField(slug_field="roomNumber", read_only = True)
     class Meta:
         model = Lesson 
-        fields = ['lessonInfo', 'lessonTime', 'inRoom_id','lessonInGroup']
+        fields = ['lessonInfo', 'lessonTime', 'inRoom']
--- timeTableProject/timeTableProject/settings.py
+++ timeTableProject/timeTableProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'table',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
